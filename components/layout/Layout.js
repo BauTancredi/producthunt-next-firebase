7c03c6f1eb3db0da1f69cdf84a79aec04cc963eb
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-07-13 18:24:33 -0300

    Testing routing

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Link from "next/link";
 
 const Layout = (props) => {
   return (
     <>
       <h1>Header</h1>
+      <nav>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/us">
+          <a>Us</a>
+        </Link>
+      </nav>
       <main>{props.children}</main>;
     </>
   );
