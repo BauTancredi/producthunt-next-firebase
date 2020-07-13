42370b4c14afcb54ae27731d4e795cdacd612b86
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-07-13 18:20:54 -0300

    Added a Layout

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Layout from "../components/layout/Layout";
 
 export default function Home() {
   return (
     <div>
-      <h1>Inicio</h1>
+      <Layout>
+        <h1>Inicio</h1>
+      </Layout>
     </div>
   );
 }
