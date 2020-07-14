bd33b84c68b23d32b98e03b4568a5a76e19d1719
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-07-14 19:16:07 -0300

    global css

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Link from "next/link";
+
 import Search from "../ui/Search";
 import Navbar from "./Navbar";
-import Link from "next/link";
 
 const Header = () => {
   return (
--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,10 +1,53 @@
 import React from "react";
+import { global, css, Global } from "@emotion/core";
 
 import Header from "./Header";
 
 const Layout = (props) => {
   return (
     <>
+      <Global
+        styles={css`
+          :root {
+            --grey: #3d3d3d;
+            --grey2: #6f6f6f;
+            --orange: #da552f;
+          }
+
+          html {
+            font-size: 62.5%;
+            box-sizing: border-box;
+          }
+
+          *,
+          *:before,
+          *:after {
+            box-sizing: inherit;
+          }
+
+          body {
+            font-size: 1.6rem;
+            line-height: 1.5;
+          }
+
+          h1,
+          h2,
+          h3 {
+            margin: 0 0 2rem 0;
+            line-height: 1.5;
+          }
+
+          ul {
+            list-style: none;
+            margin: 0;
+            padding: 0;
+          }
+
+          a {
+            text-decoration: none;
+          }
+        `}
+      />
       <Header />
       <main>{props.children}</main>
     </>
