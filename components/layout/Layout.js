48e7f9e830466ba8ceefb09ded8cfcf66bf60375
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-07-15 09:55:32 -0300

    google fonts and images

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { global, css, Global } from "@emotion/core";
+import Head from "next/head";
 
 import Header from "./Header";
 
@@ -28,6 +29,7 @@ const Layout = (props) => {
           body {
             font-size: 1.6rem;
             line-height: 1.5;
+            font-family: "PT Sans", sans-serif;
           }
 
           h1,
@@ -36,7 +38,14 @@ const Layout = (props) => {
             margin: 0 0 2rem 0;
             line-height: 1.5;
           }
-
+          h1,
+          h2 {
+            font-family: "Roboto Slab", serif;
+            font-weight: 700;
+          }
+          h3 {
+            font-family: "PT Sans", sans-serif;
+          }
           ul {
             list-style: none;
             margin: 0;
@@ -48,6 +57,22 @@ const Layout = (props) => {
           }
         `}
       />
+
+      <Head>
+        <html lang="es" />
+        <title>Product Hunt. Firebase & Next.js</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+          crossorigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+        <link href="/static/css/app.css" rel="stylesheet" />
+      </Head>
       <Header />
       <main>{props.children}</main>
     </>
