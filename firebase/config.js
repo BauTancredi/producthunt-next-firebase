5ce516aa68e839ee743966272abd0f66d6c788a7
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-07-17 15:15:00 -0300

    Adding firebase

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCM0UmIH2Gs8accd6ouHEuMjLYxCKe0Prk",
+  authDomain: "producthunt-f6140.firebaseapp.com",
+  databaseURL: "https://producthunt-f6140.firebaseio.com",
+  projectId: "producthunt-f6140",
+  storageBucket: "producthunt-f6140.appspot.com",
+  messagingSenderId: "74678099565",
+  appId: "1:74678099565:web:b3eed38fd1e50cffc56964",
+};
+
+export default firebaseConfig;
