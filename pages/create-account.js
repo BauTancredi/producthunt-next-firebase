9518c56d9044154662d2ea2c4836ee154a832d6e
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-07-17 14:08:35 -0300

    Create account form

--- pages/create-account.js
+++ pages/create-account.js
@@ -1,11 +1,54 @@
 import React from "react";
+import { css } from "@emotion/core";
+
 import Layout from "../components/layout/Layout";
+import { Form, Field, InputSubmit } from "../components/ui/Form";
 
 export default function CreateAccount() {
   return (
     <div>
       <Layout>
-        <h1>CreateAccount</h1>
+        <>
+          <h1
+            css={css`
+              text-align: center;
+              margin-top: 5rem;
+            `}
+          >
+            Create Account
+          </h1>
+          <Form>
+            <Field>
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                id="name"
+                placeholder="Your Name"
+                name="name"
+              />
+            </Field>
+            <Field>
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Your Email"
+                name="email"
+              />
+            </Field>
+            <Field>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Your Password"
+                name="password"
+              />
+            </Field>
+
+            <InputSubmit type="submit" value="Create account" />
+          </Form>
+        </>
       </Layout>
     </div>
   );
