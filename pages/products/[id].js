66cd90987d437cbad0d91fc2147a8949d4943e1b
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-07-20 18:51:03 -0300

    404 component

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,7 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { useRouter } from "next/router";
 
+import Layout from "../../components/layout/Layout";
+import { FirebaseContext } from "../../firebase";
+import Error404 from "../../components/layout/404";
+
 const Product = () => {
+  const [product, setProduct] = useState({});
+  const [error, setError] = useState(false);
+
   // Routing for getting the actual id
   const router = useRouter();
 
@@ -9,12 +16,25 @@ const Product = () => {
     query: { id },
   } = router;
 
+  const { firebase } = useContext(FirebaseContext);
+
   useEffect(() => {
     if (id) {
+      const getProduct = async () => {
+        const productQuery = await firebase.db.collection("products").doc(id);
+        const product = await productQuery.get();
+        if (product.exists) setProduct(product.data());
+        else setError(true);
+      };
+      getProduct();
     }
   }, [id]);
 
-  return <h1>{id}</h1>;
+  return (
+    <Layout>
+      <>{error && <Error404 />}</>
+    </Layout>
+  );
 };
 
 export default Product;
