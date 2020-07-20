176825c6433eff4123d92423d13edb276c698f9e
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-07-20 19:21:22 -0300

    Displaying content and comments

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,9 +1,28 @@
 import React, { useEffect, useContext, useState } from "react";
 import { useRouter } from "next/router";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+import styled from "@emotion/styled";
 
 import Layout from "../../components/layout/Layout";
 import { FirebaseContext } from "../../firebase";
 import Error404 from "../../components/layout/404";
+import { Field, InputSubmit } from "../../components/ui/Form";
+
+const H1 = styled.h1`
+  text-align: center;
+  margin-top: 5rem;
+`;
+const H2 = styled.h2`
+  margin: 2rem 0;
+`;
+
+const ProductContainter = styled.div`
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    column-gap: 2rem;
+  }
+`;
 
 const Product = () => {
   const [product, setProduct] = useState({});
@@ -30,9 +49,52 @@ const Product = () => {
     }
   }, [id]);
 
+  if (Object.keys(product).length === 0) return "Loading...";
+
+  const {
+    comments,
+    created,
+    name,
+    company,
+    url,
+    image,
+    urlImage,
+    votes,
+    description,
+  } = product;
+
   return (
     <Layout>
-      <>{error && <Error404 />}</>
+      <>
+        {error && <Error404 />}
+        <div className="contenedor">
+          <H1>{name}</H1>
+          <ProductContainter>
+            <div>
+              <p>Published {formatDistanceToNow(new Date(created))} ago</p>
+              <img src={urlImage} />
+              <p>{description}</p>
+
+              <h2>Add comments</h2>
+              <form>
+                <Field>
+                  <input type="text" name="message" />
+                </Field>
+                <InputSubmit type="submit" value="Add comment" />
+              </form>
+
+              <H2>Comments</H2>
+              {comments.map((comment) => (
+                <li>
+                  <p>{comment.name}</p>
+                  <p>Writed by: {comment.userName}</p>
+                </li>
+              ))}
+            </div>
+            <aside></aside>
+          </ProductContainter>
+        </div>
+      </>
     </Layout>
   );
 };
