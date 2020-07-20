3c262f3baaf2c187426739ea7d022053cbca043f
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-07-19 21:29:56 -0300

    query db

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
 import Layout from "../components/layout/Layout";
+import { FirebaseContext } from "../firebase";
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const obtainProducts = () => {
+      firebase.db
+        .collection("products")
+        .orderBy("created", "desc")
+        .onSnapshot(manageSnapshot);
+    };
+    obtainProducts();
+  }, []);
+
+  function manageSnapshot(snapshot) {
+    const products = snapshots.docs.map((doc) => {
+      return {
+        id: doc.id,
+        ...doc.data(),
+      };
+    });
+
+    setProducts(products);
+  }
+
   return (
     <div>
       <Layout>
