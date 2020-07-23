9fd630da34c7a34616d3acb9c170a85b7ebacf23
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-07-23 18:51:32 -0300

    Search bar

--- pages/search.js
+++ pages/search.js
@@ -1,12 +1,47 @@
-import React from "react";
-import Layout from "../components/layout/Layout";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
-export default function Search() {
+import useProducts from "../hooks/useProducts";
+
+import Layout from "../components/layout/Layout";
+import ProductDetails from "../components/layout/ProductDetails";
+
+const Search = () => {
+  const router = useRouter();
+
+  const {
+    query: { q },
+  } = router;
+
+  const { products } = useProducts("created");
+  const [result, setResult] = useState([]);
+  useEffect(() => {
+    const search = q.toLowerCase();
+    const filter = products.filter((product) => {
+      return (
+        product.name.toLowerCase().includes(search) ||
+        product.description.toLowerCase().includes(search)
+      );
+    });
+
+    setResult(filter);
+  }, [q, products]);
+
   return (
     <div>
       <Layout>
-        <h1>Search</h1>
+        <div className="listado-productos">
+          <div className="contenedor">
+            <div className="bg-white">
+              {result.map((product) => (
+                <ProductDetails key={product.id} product={product} />
+              ))}
+            </div>
+          </div>
+        </div>
       </Layout>
     </div>
   );
-}
+};
+
+export default Search;
